ab7264135269eac7cb85d5d0d9f5942fb9f05e38
Harith Yerragolam <yharith16@gmail.com>
2025-02-07 00:38:58 +0530

    feat: add Block component and implement game start functionality in App.vue

---
+++ src/components/Block.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="block">
+    Click me
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['delay']
+}
+</script>
+
+<style>
+    .block {
+        width: 400px;
+        border-radius: 20px;
+        background: #0faf87;
+        color: white;
+        padding: 100px 0;
+        text-align: center;
+        margin: 40px auto;
+        transition: background-color 0.5s;
+    }
+</style>
